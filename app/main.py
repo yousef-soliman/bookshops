89e64b33880f3ae09d41a9cdc4b0e5c2bb47d7b0
Yousef Soliman <yousef.soliman02@gmail.com>
2024-02-03 21:07:24 +0200

    handle value error exception

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
 
 
 from app.author.routers import router as author_router
@@ -9,3 +10,11 @@ app = FastAPI(debug=True)
 app.include_router(author_router)
 app.include_router(book_router)
 app.include_router(store_router)
+
+
+@app.exception_handler(ValueError)
+async def value_error_exception_handler(request: Request, exc: ValueError):
+    return JSONResponse(
+        status_code=400,
+        content={"message": str(exc)},
+    )
